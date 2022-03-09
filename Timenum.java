5b3fba5bb2100c342ea535302a9dcc0f9f350def
岩崎 <karausuyanosegare@gmail.com>
2022-03-09 17:40:46 +0900

    #2 print部分を::にしていた

--- Timenum.java
+++ Timenum.java
@@ -6,6 +6,6 @@ public class Timenum {
     public static void main(String[] args) {
     LocalTime time =LocalTime.now();
     IntStream stream = IntStream.range(1,10000);
-    stream.forEach(num -> System.out::println(time+" "+num;));
+    stream.forEach(num -> System.out.println(time+" "+num));
     }
 }
