76fb25bb78b09bfe1ecb4f2cd991c19b1bebecf2
岩崎 <karausuyanosegare@gmail.com>
2022-03-09 17:34:34 +0900

    #2 課題２の出力する文章の連結ができない

--- Timenum.java
+++ Timenum.java
@@ -1,8 +1,11 @@
 import java.util.stream.IntStream;
+import java.time.LocalTime;
+
 
 public class Timenum {
     public static void main(String[] args) {
-    IntStream stream = IntStream.range(1,999);
-    stream.forEach(System.out::println);
+    LocalTime time =LocalTime.now();
+    IntStream stream = IntStream.range(1,10000);
+    stream.forEach(num -> System.out::println(time+" "+num;));
     }
 }
